49a1d04a0ad60a6040ae981c7cfa4518c2d71160
Sai Krishna Reddy Katta <saikrishnareddykatta@Sais-MacBook-Pro.local>
2023-10-24 20:41:25 -0700

    feat(d3update): real time database updates

--- 09-d3_realtime_update_pattern/index.js
+++ 09-d3_realtime_update_pattern/index.js
@@ -76,24 +76,25 @@ const update = (data) => {
   yAxisGroup.call(yAxis);
 };
 
-db.collection("dishes")
-  .get()
-  .then((res) => {
-    let data = [];
-    res.docs.forEach((doc) => data.push(doc.data()));
-    update(data);
-    d3.interval(() => {
-      // data[0].orders += 50; // this will increase the orders of first category by 50 for every 2 seconds
-      data.pop(); // this will remove the categories one by one for every 2 seconds
-      update(data);
-    }, 2000);
-  })
-  .catch(() => {
-    graph
-      .append("text")
-      .attr("x", 150)
-      .attr("y", 300)
-      .attr("fill", "black")
-      .text("Unable to load menu.json file")
-      .style("font-family", "arial");
+var data = [];
+
+db.collection("dishes").onSnapshot((res) => {
+  res.docChanges().forEach((change) => {
+    const doc = { ...change.doc.data(), id: change.doc.id };
+    switch (change.type) {
+      case "added":
+        data.push(doc);
+        break;
+      case "modified":
+        const index = data.findIndex((item) => item.id === doc.id);
+        data[index] = doc;
+        break;
+      case "removed":
+        data = data.filter((item) => item.id !== doc.id);
+        break;
+      default:
+        break;
+    }
   });
+  update(data);
+});
